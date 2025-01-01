3e70284b573880c935d0bb0690fbc782299342fb
Tobias Bossert <tobias@dufour.aero>
2025-01-01 12:36:13 +0100

    added missing files

---
+++ raspi-alarm/pir_sensor/motion_detect.py
@@ -0,0 +1,11 @@
+import pigpio
+from typing import Callable
+INTERRUPT_PIN = 17
+
+class Pir:
+
+    def __init__(self, interrupt_pin, pi: pigpio.pi, callback_fn: Callable) -> None:
+        self.pi = pi
+        self.interrupt_pin = interrupt_pin
+        pi.set_mode(INTERRUPT_PIN, pigpio.INPUT)
+        pi.callback(INTERRUPT_PIN, pigpio.RISING_EDGE, callback_fn)
